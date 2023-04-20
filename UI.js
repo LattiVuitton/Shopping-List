02503bd7e37319c4ea2e6c24599db84a4097cc7f
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-04-21 00:04:52 +1000

    Main pages

---
+++ UI.js
@@ -0,0 +1,14 @@
+function startTime() {
+	var startDate = new Date();
+	setInterval(function() {
+		var currentDate = new Date();
+		var elapsedTime = currentDate.getTime() - startDate.getTime();
+		var seconds = Math.floor((elapsedTime / 1000) % 60);
+		var minutes = Math.floor((elapsedTime / 1000 / 60) % 60);
+		var hours = Math.floor((elapsedTime / 1000 / 60 / 60) % 24);
+		var days = Math.floor(elapsedTime / 1000 / 60 / 60 / 24);
+
+		var timer = document.getElementById('timer');
+		timer.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+	}, 1000);
+}
