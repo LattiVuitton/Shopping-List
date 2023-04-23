d4b3d2a8c551d85c5d8b077f4de85af9679c7c58
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-04-23 16:33:09 +1000

    added Settings button

--- UI.js
+++ UI.js
@@ -1,21 +1,57 @@
-var itemText = document.getElementById("itemText");
+const itemText = document.getElementById("itemText");
+const clearButton = document.getElementById("clear");
+const settingsButton = document.getElementById("settings");
+
+var items = []
+var itemElements = []
+
+class Item {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+function clear() {
+    console.log("Clearing")
+    document.querySelectorAll('.ingredient').forEach(e => e.remove());
+    items = []
+}
 
 function createItem() {
     const itemsList = document.getElementById("items");
 
     let text = itemText.value;
 
+    const newItem = new Item(text);
+    items.push(newItem);
+    console.log(items)
+
     const newP = document.createElement('p');
     newP.innerHTML = text + "";
+    newP.className = "ingredient"
     itemsList.append(newP);
 
     itemText.value = ""
+    newP.style.backgroundColor = "red";
 }
 
 
 
 itemText.addEventListener("keydown", function (e) {
-    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+    if (e.code === "Enter") {
         createItem();
     }
 });
+
+clearButton.addEventListener("click", function () {
+    clear()
+})
+
+
+function slidePanel() {
+    console.log("Sliding")
+}
+
+settingsButton.addEventListener("click", function () {
+    slidePanel();
+})
